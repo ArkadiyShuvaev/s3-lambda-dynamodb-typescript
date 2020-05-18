2a86771b7b2c5e45d53d4fc97b9929dca1c40c16
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2020-05-18 22:24:00 +0200

    modified gitignore

--- .aws-sam/build/S3Handler/app.ts
+++
@@ -1,8 +0,0 @@
-//import S3 from "aws-sdk/clients/s3";
-// import * as AwsLambda from "aws-lambda";
-import { Context, S3Event, } from "aws-lambda";
-
-export function s3Handler(event: S3Event, context: Context): void {
-    console.log(event);
-    console.log(context);
-}
--- my-app/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function s3Handler(event, context) {
-    console.log(event);
-    console.log(context);
-}
-exports.s3Handler = s3Handler;
-//# sourceMappingURL=app.js.map
