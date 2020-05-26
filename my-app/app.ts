5daa255b373111cc91440c0843b1b7a2f50019fc
Arkadiy Shuvaev <shuvaev.arkady@gmail.com>
2020-05-26 22:49:41 +0200

    removed console.debug

--- my-app/app.ts
+++ my-app/app.ts
@@ -6,8 +6,6 @@ import S3FileInfo from "./s3FileInfo";
 export function s3Handler(event: S3Event, context: Context, callback: Callback): void {
     const s3ObjKey = event.Records[0].s3.object.key;
     const s3ObjTime = event.Records[0].eventTime;
-    console.log(event);
-    console.log(context);
 
     const s3FileInfo = new S3FileInfo(s3ObjKey, s3ObjTime);
     s3FileInfo.save();
